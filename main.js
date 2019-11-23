06320bfe289acaf5def49df8f0600a5652543560
KevinW <bird831205@hotmail.com>
2019-11-23 10:49:20 -0500

    typescript types

--- main.js
+++ main.js
@@ -6,17 +6,38 @@
 // var x=1;
 // let y = 2;
 // const z =3;
-function doSomething() {
-    for (var i = 0; i < 5; i++) {
-        console.log(i);
-    }
-    console.log("Var Finally: " + i);
-}
-function doSomething1() {
-    for (var i = 0; i < 5; i++) {
-        console.log(i);
-    }
-    console.log("let Finally: " + i);
-}
-doSomething();
-doSomething1();
+// function doSomething (){
+//     for(var i =0; i<5; i++){
+//         console.log(i);
+//     }
+//     console.log("Var Finally: "+i)
+// }
+// function doSomething1 (){
+//     for(let i =0; i<5; i++){
+//         console.log(i);
+//     }
+//     console.log("let Finally: "+ i +" but with compile error")
+// }
+// doSomething();
+// doSomething1();
+// let count = 5;
+// count = "a";   // typescript has type
+var a; // if didn't declare type might have issues
+a = 1;
+a = true;
+// let b: number; //declare type to preven issue
+// b = 1;
+// b = true;
+var c;
+var d;
+var e;
+var f;
+var g;
+var Color;
+(function (Color) {
+    Color[Color["Red"] = 0] = "Red";
+    Color[Color["Green"] = 1] = "Green";
+    Color[Color["Blue"] = 2] = "Blue";
+})(Color || (Color = {}));
+; // better to explicitly set the value of enum.
+var backgroundColor = Color.Red;
--- main.ts
+++ main.ts
@@ -10,21 +10,42 @@
 // let y = 2;
 // const z =3;
 
-function doSomething (){
-    for(var i =0; i<5; i++){
-        console.log(i);
-    }
+// function doSomething (){
+//     for(var i =0; i<5; i++){
+//         console.log(i);
+//     }
 
-    console.log("Var Finally: "+i)
-}
+//     console.log("Var Finally: "+i)
+// }
 
-function doSomething1 (){
-    for(let i =0; i<5; i++){
-        console.log(i);
-    }
+// function doSomething1 (){
+//     for(let i =0; i<5; i++){
+//         console.log(i);
+//     }
 
-    console.log("let Finally: "+ i +" but with compile error")
-}
+//     console.log("let Finally: "+ i +" but with compile error")
+// }
 
-doSomething();
-doSomething1();
+// doSomething();
+// doSomething1();
+
+// let count = 5;
+// count = "a";   // typescript has type
+
+let a;      // if didn't declare type might have issues
+a = 1;
+a = true;
+
+// let b: number; //declare type to preven issue
+// b = 1;
+// b = true;
+
+let c: boolean;
+let d: string;
+let e: any;
+let f: number[];
+let g: any[];
+
+enum Color { Red = 0, Green = 1, Blue = 2 }; // better to explicitly set the value of enum.
+let backgroundColor = Color.Red;
+
