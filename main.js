fd27a85ebd9de19d7fc0ff9f3d6b7c281526423b
KevinW <bird831205@hotmail.com>
2019-11-23 10:42:20 -0500

    let var scope and error catching

--- main.js
+++ main.js
@@ -1,5 +1,22 @@
-function log(message) {
-    console.log(message);
+// function log(message){
+//     console.log(message);
+// }
+// var message = "hello world";
+// log(message);
+// var x=1;
+// let y = 2;
+// const z =3;
+function doSomething() {
+    for (var i = 0; i < 5; i++) {
+        console.log(i);
+    }
+    console.log("Var Finally: " + i);
 }
-var message = "hello world";
-log(message);
+function doSomething1() {
+    for (var i = 0; i < 5; i++) {
+        console.log(i);
+    }
+    console.log("let Finally: " + i);
+}
+doSomething();
+doSomething1();
--- main.ts
+++ main.ts
@@ -1,8 +1,30 @@
 
-function log(message){
-    console.log(message);
+// function log(message){
+//     console.log(message);
+// }
+
+// var message = "hello world";
+// log(message);
+
+// var x=1;
+// let y = 2;
+// const z =3;
+
+function doSomething (){
+    for(var i =0; i<5; i++){
+        console.log(i);
+    }
+
+    console.log("Var Finally: "+i)
 }
 
-var message = "hello world";
+function doSomething1 (){
+    for(let i =0; i<5; i++){
+        console.log(i);
+    }
 
-log(message);
+    console.log("let Finally: "+ i +" but with compile error")
+}
+
+doSomething();
+doSomething1();
